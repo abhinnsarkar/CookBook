b96b5f2c5dca947af2fa323b4e56d9d55b5bcd00
Abhinn Sarkar <abhinnsarkar@Abhinns-MacBook-Pro.local>
2023-03-16 10:28:47 -0700

    For backend, fixed port issue and initialized frontend

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const connectDB = require("./config/db");
 
+const config = require("config");
+
+const configPORT = config.get("PORT");
+
 const app = express();
 connectDB();
 app.get("/", (req, res) => {
@@ -13,7 +17,8 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/recipes", require("./routes/api/recipes"));
 
-const PORT = 4000;
+const PORT = configPORT || 1000;
+
 app.listen(PORT, () => {
   console.log(`Server listening on Port ${PORT}`);
 });
